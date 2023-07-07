5f7fcbf7b2a3aa8d4ea713a24ef9079c354ba61c
JakrapunS <jakrapun.san@gmail.com>
2023-07-08 00:08:49 +1000

    add load_model

--- app.py
+++ app.py
@@ -29,12 +29,18 @@ def summarize_text(url,model, tokenizer):
 
     return summary_text
 
-# Streamlit app
-def main():
+#add cathc load model
+@st.cache
+def load_model():
     model_name = "facebook/bart-large-xsum" 
     # Download pytorch model
     tokenizer = BartTokenizer.from_pretrained(model_name)
     model = BartForConditionalGeneration.from_pretrained(model_name)
+    return model,tokenizer
+
+# Streamlit app
+def main():
+    model,tokenizer = load_model()
 
     st.title("Webpage Text Summarizer")
     st.write("Enter a URL and get a summary of the webpage text.")
