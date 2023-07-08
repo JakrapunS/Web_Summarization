2d341d28ec8dd426a7d59f3209b5d520e930cf93
JakrapunS <jakrapun.san@gmail.com>
2023-07-08 21:53:10 +1000

    T5 Implementation

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-from transformers import BartTokenizer, BartForConditionalGeneration
+#from transformers import BartTokenizer, BartForConditionalGeneration
+from transformers import pipeline
+import torch
 
 
-def summarize_text(url,model, tokenizer):
+
+def summarize_text(url,summarizer, params):
     # facebook/bart-large-xsum
     
     # https://arxiv.org/abs/1910.13461
@@ -13,67 +16,34 @@ def summarize_text(url,model, tokenizer):
     # Create a BeautifulSoup object to parse the HTML content
     soup = BeautifulSoup(response.content, 'html.parser')
     text = soup.text
-    #transformed_text = re.sub(r'[^ ]', r'\\', text)
-    max_length = 1024
 
-    text_len = len(text)
+    result = summarizer(text, **params)
 
-    n = round(text_len/max_length)
-    # Create text chunk
-    chunk=[]
-    for i in range(n):
-        start = (i*max_length)
-        end = (i+1)*max_length
-        if end > text_len:
-            end = text_len
-            
-        chunk.append(text[start:end])
+    return result[0]['summary_text']
 
-    #print(truncated_input)
-    # Transform input tokens 
-
-    result_text = []
-    for text in chunk:
-        inputs = tokenizer.encode(text, return_tensors="pt")
-
-        # Generate summary
-        summary_ids = model.generate(inputs, num_beams=4, max_length=1024, early_stopping=True)
-
-        # Convert summary IDs to text
-        summary_text = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
-
-        result_text.append(summary_text)
-
-    concatenated_text = ' '.join(result_text)
-
-    if len(concatenated_text)  > max_length:
-        final = concatenated_text[:max_length]
-    else:
-        final =concatenated_text
-    
-    #final summarization
-    #inputs_final = tokenizer.encode(final, return_tensors="pt")
-
-    # Generate summary
-    #summary_ids_final = model.generate(inputs_final, num_beams=4, max_length=1024, early_stopping=True)
-
-    # Convert summary IDs to text
-    #summary_text_final = tokenizer.decode(summary_ids_final.squeeze(), skip_special_tokens=True)
-
-    return concatenated_text
-
-#add cathc load model
 
 def load_model():
-    model_name = "facebook/bart-large-xsum" 
-    # Download pytorch model
-    tokenizer = BartTokenizer.from_pretrained(model_name)
-    model = BartForConditionalGeneration.from_pretrained(model_name)
-    return model,tokenizer
+    summarizer = pipeline(
+        "summarization",
+        "pszemraj/long-t5-tglobal-base-16384-book-summary",
+        device=0 if torch.cuda.is_available() else -1,
+    )
+    params = {
+        "max_length": 1024,
+        "min_length": 56,
+        "no_repeat_ngram_size": 3,
+        "early_stopping": True,
+        "repetition_penalty": 3.5,
+        "length_penalty": 0.3,
+        "encoder_no_repeat_ngram_size": 3,
+        "num_beams": 4,
+    } # parameters for text generation out of model
+    return summarizer,params
+
 
 # Streamlit app
 def main():
-    model,tokenizer = load_model()
+    summarizer,params = load_model()
 
     st.title("Webpage Text Summarizer")
     st.write("Enter a URL and get a summary of the webpage text.")
@@ -81,7 +51,7 @@ def main():
     url = st.text_input("Enter URL:")
     if url:
         try:
-            summary = summarize_text(url,model,tokenizer)
+            summary = summarize_text(url,summarizer,params)
             st.subheader("Summary:")
             st.write(summary)
         except:
