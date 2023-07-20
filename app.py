83600850ef97e6c51608f8dcf626c17116307a46
JakrapunS <jakrapun.san@gmail.com>
2023-07-21 01:27:05 +1000

    Add text chunk using lang chain

--- app.py
+++ app.py
@@ -1,66 +1,60 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-#from transformers import BartTokenizer, BartForConditionalGeneration
-from transformers import pipeline
+
+from langchain import HuggingFacePipeline
+from transformers import AutoTokenizer,pipeline, AutoModelForSeq2SeqLM
+import transformers
 import torch
+#from langchain import PromptTemplate,  LLMChain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.chains.summarize import load_summarize_chain
 
 
-def answer_question(qa_pipeline, question, context):
 
+
+def summarize_text(url,model):
     
-    res = qa_pipeline(question,context)
-    return res['answer']
-
-
-def summarize_text(url,summarizer, params):
-    # LongT5
-    
-    # https://arxiv.org/abs/1910.13461
     url = url
     response = requests.get(url)
     # Create a BeautifulSoup object to parse the HTML content
     soup = BeautifulSoup(response.content, 'html.parser')
     text = soup.text
 
-    result = summarizer(text, **params)
-    print(result[0].keys())
-
-    return result[0]['summary_text'],text
+    text_splitter = CharacterTextSplitter()
+    texts = text_splitter.split_text(text)
+    # Create multiple documents
+    docs = [Document(page_content=t) for t in texts]
+    
+    #template = """
+    #          Write a concise summary of the following text.
+    #          ```{text}```
+    #       """
+    #prompt = PromptTemplate(template=template, input_variables=["text"])
+    #llm_chain = LLMChain(prompt=prompt, llm=model)
+    chain = load_summarize_chain(model, chain_type='map_reduce')
+    return chain.run(docs), text
 
 
 def load_models():
-    summarizer = pipeline(
-        "summarization",
-        "pszemraj/long-t5-tglobal-base-16384-book-summary",
-        device=0 if torch.cuda.is_available() else -1,
+    model = 'google/flan-t5-small'
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    #model = AutoModelForSeq2SeqLM.from_pretrained(model, load_in_8bit=True)
+
+    pipe = pipeline(
+        "text2text-generation",
+        model=model, 
+        tokenizer=tokenizer, 
+        max_length=100
     )
-
-
-    #https://arxiv.org/abs/1910.01108
-    #roberta
-    qa_pipeline = pipeline(
-        'question-answering',
-        "deepset/roberta-base-squad2",
-        device=0 if torch.cuda.is_available() else -1,
-        )
-
-    params = {
-        "max_length": 1024,
-        "min_length": 8,
-        "no_repeat_ngram_size": 3,
-        "early_stopping": True,
-        "repetition_penalty": 3.5,
-        "length_penalty": 0.1,
-        "encoder_no_repeat_ngram_size": 3,
-        "num_beams": 4,
-    } # parameters for text generation out of model
-    return summarizer,params,qa_pipeline
+    local_llm = HuggingFacePipeline(pipeline=pipe)
+    return local_llm
 
 
 # Streamlit app
 def main():
-    summarizer, summarizer_params, qa_pipeline = load_models()
+    llm = load_models()
 
     st.title("Webpage Text Summarizer and Question Answering")
     st.write("Enter a URL and get a summary of the webpage text. You can also ask questions about the content.")
@@ -74,22 +68,12 @@ def main():
     # Input URL
     url = st.text_input("Enter URL:")
     if url:
-        try:
-            summary,text = summarize_text(url, summarizer, summarizer_params)
-            st.subheader("Summary:")
-            st.write(summary)
+        
+        summary,text = summarize_text(url, llm)
+        st.subheader("Summary:")
+        st.write(summary)
 
-            # Allow user to ask questions
-            st.subheader("Question Answering:")
-            question = st.text_input("Ask a question:")
-            if question:
-                try:
-                    answer = answer_question(qa_pipeline, question, text)
-                    st.write("Answer:", answer)
-                except:
-                    st.write("Error: Failed to answer the question.")
-        except:
-            st.write("Error: Failed to summarize the text. Please check the URL or try again later.")
+        
 
 if __name__ == "__main__":
     main()
